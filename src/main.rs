9bf85fc4c7062e4af78a6796e508162782d10e0b
Une Nyide <une.nyide@gmail.com>
2025-03-11 23:12:55 +0200

    add basic boilerplate to start desktop app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use eframe::*;
+use egui::*;
+
+struct MyApp {}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello World 🚀");
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Launching 🚀");
 }
