7f1f2efdd77512c9a555d30cc475923641f7610d
Une Nyide <une.nyide@gmail.com>
2025-04-20 12:43:53 +0200

    feat: improve line crispness

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -14,22 +14,22 @@ onMounted(() => {
 
   ctx.fillStyle = "orange";
   ctx.strokeStyle = "grey";
-  ctx.lineWidth = 2;
+  ctx.lineWidth = 1;
   ctx.lineJoin = "round";
 
   for (let x = 0; x <= width / CELL_WIDTH; x++) {
-    const x_ordinate = x * CELL_WIDTH;
-    ctx.moveTo(x_ordinate, 0)
+    const x_ordinate = ctx.lineWidth % 2 === 0 ? x * CELL_WIDTH : (x * CELL_WIDTH) + 0.5;
+    ctx.moveTo(x_ordinate, 0);
     ctx.lineTo(x_ordinate, height);
-    for (let y = 0; y <= height / CELL_WIDTH; y++) {
-      const y_ordinate = y * CELL_WIDTH;
-      ctx.moveTo(0, y_ordinate);
-      ctx.lineTo(width, y_ordinate);
-    }
+
+  }
+  for (let y = 0; y <= height / CELL_WIDTH; y++) {
+    const y_ordinate = ctx.lineWidth % 2 === 0 ? y * CELL_WIDTH : (y * CELL_WIDTH) + 0.5;
+    ctx.moveTo(0, y_ordinate);
+    ctx.lineTo(width, y_ordinate);
   }
 
   ctx.stroke();
-
   ctx.scale(devicePixelRatio, devicePixelRatio);
 
   canvas.style.width = `${clientWidth}px`;
