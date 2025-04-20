2b842ac70b3e0b3703458ef7ee1c0b1d0c563eeb
Une Nyide <une.nyide@gmail.com>
2025-04-20 11:51:53 +0200

    feat: add css reset

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+const { clientWidth, clientHeight } = document.querySelector("body")!;
+const width = clientWidth * devicePixelRatio, height = clientHeight * devicePixelRatio;
+
 </script>
 
 <template>
-
+  <canvas id="playground" :width="width" :height="height" />
 </template>
 
-<style scoped></style>
+<style scoped>
+#playground {
+  border: solid 10px black;
+}
+</style>
