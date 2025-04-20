6fc8ab91fabb5ff589d807a525f5e6d12260be04
Une Nyide <une.nyide@gmail.com>
2025-04-20 12:09:37 +0200

    feat: add downsampling to canvas

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,11 +1,36 @@
 <script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue';
+
 const { clientWidth, clientHeight } = document.querySelector("body")!;
 const width = clientWidth * devicePixelRatio, height = clientHeight * devicePixelRatio;
+const canvasRef = useTemplateRef<HTMLCanvasElement>("playground")
 
+onMounted(() => {
+  if (!canvasRef.value) return;
+  const canvas = canvasRef.value;
+  const ctx = canvas.getContext("2d")!;
+
+  ctx.fillStyle = "orange";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 5;
+  ctx.lineJoin = "round";
+  ctx.moveTo(100, 100);
+  ctx.lineTo(200, 100);
+  ctx.lineTo(200, 200);
+  ctx.lineTo(100, 200);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.scale(devicePixelRatio, devicePixelRatio);
+
+  canvas.style.width = `${clientWidth}px`;
+  canvas.style.height = `${clientHeight}px`;
+})
 </script>
 
 <template>
-  <canvas id="playground" :width="width" :height="height" />
+  <canvas id="playground" :width="width" :height="height" ref="playground" />
 </template>
 
 <style scoped>
